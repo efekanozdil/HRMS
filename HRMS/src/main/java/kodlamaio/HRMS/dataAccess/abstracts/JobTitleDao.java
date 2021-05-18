d526f43f6b5b62594b01728520f07a9222a1ad9f
Efekan Ozdil <efekanozdil@gmail.com>
2021-05-18 10:13:09 +0300

    Project

---
+++ HRMS/src/main/java/kodlamaio/HRMS/dataAccess/abstracts/JobTitleDao.java
@@ -0,0 +1,10 @@
+package kodlamaio.HRMS.dataAccess.abstracts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import kodlamaio.HRMS.entities.concretes.JobTitle;
+
+public interface JobTitleDao extends JpaRepository<JobTitle, Integer>{
+	
+
+}
